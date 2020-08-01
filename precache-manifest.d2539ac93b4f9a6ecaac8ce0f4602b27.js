f47be8e9b6d46cbf7b0455b3a6e9299b6aa72f9d
Ros McMahon <rosmcmahon@gmail.com>
2020-08-01 13:15:51 +0100

    Updates

---
+++ precache-manifest.d2539ac93b4f9a6ecaac8ce0f4602b27.js
@@ -0,0 +1,406 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931d857e2f95c141a110ec8840dce470",
+    "url": "/arweave-id-v2-ui/index.html"
+  },
+  {
+    "revision": "a3e3c147bee5d5666d4d",
+    "url": "/arweave-id-v2-ui/static/css/10.bf8e508d.chunk.css"
+  },
+  {
+    "revision": "cbf928177bab0f13adfd",
+    "url": "/arweave-id-v2-ui/static/css/main.12b0eddc.chunk.css"
+  },
+  {
+    "revision": "7758472efcb965633001",
+    "url": "/arweave-id-v2-ui/static/js/0.16b91da4.chunk.js"
+  },
+  {
+    "revision": "f8b625f7b0080232a2e6",
+    "url": "/arweave-id-v2-ui/static/js/1.75c662a3.chunk.js"
+  },
+  {
+    "revision": "a3e3c147bee5d5666d4d",
+    "url": "/arweave-id-v2-ui/static/js/10.069c1f40.chunk.js"
+  },
+  {
+    "revision": "85a952f1e77e912d06cc63ed208b9b51",
+    "url": "/arweave-id-v2-ui/static/js/10.069c1f40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b9badeae8818756a71",
+    "url": "/arweave-id-v2-ui/static/js/11.68811396.chunk.js"
+  },
+  {
+    "revision": "4f0bceace12658407ebf",
+    "url": "/arweave-id-v2-ui/static/js/12.9d8ea698.chunk.js"
+  },
+  {
+    "revision": "ffc2fc0f4683967bd865",
+    "url": "/arweave-id-v2-ui/static/js/13.a5ec2faf.chunk.js"
+  },
+  {
+    "revision": "86edff497f1e3fd220b2",
+    "url": "/arweave-id-v2-ui/static/js/14.0347d26e.chunk.js"
+  },
+  {
+    "revision": "d39c6fd8987bc13decdc",
+    "url": "/arweave-id-v2-ui/static/js/15.d19b6d9a.chunk.js"
+  },
+  {
+    "revision": "5a7939571ff6085d2c4b",
+    "url": "/arweave-id-v2-ui/static/js/16.d61bd63e.chunk.js"
+  },
+  {
+    "revision": "d49bd5517e87aeb69e9b",
+    "url": "/arweave-id-v2-ui/static/js/17.c29ca8e1.chunk.js"
+  },
+  {
+    "revision": "17da53ef43aec5e65e7e",
+    "url": "/arweave-id-v2-ui/static/js/18.676639df.chunk.js"
+  },
+  {
+    "revision": "ec22e8ff9186343ee67d",
+    "url": "/arweave-id-v2-ui/static/js/19.098f2a26.chunk.js"
+  },
+  {
+    "revision": "e8328eb5f0b7d9efc678",
+    "url": "/arweave-id-v2-ui/static/js/2.7df67330.chunk.js"
+  },
+  {
+    "revision": "37ca09defad2c72534da",
+    "url": "/arweave-id-v2-ui/static/js/20.2392d98f.chunk.js"
+  },
+  {
+    "revision": "ff150372accdd03e1d1c",
+    "url": "/arweave-id-v2-ui/static/js/21.3cbbaee1.chunk.js"
+  },
+  {
+    "revision": "307604823fc1327b3e5c",
+    "url": "/arweave-id-v2-ui/static/js/22.c10f0450.chunk.js"
+  },
+  {
+    "revision": "87f372ced5bb3820273a",
+    "url": "/arweave-id-v2-ui/static/js/23.5a2554ed.chunk.js"
+  },
+  {
+    "revision": "23be0734d77444b432dc",
+    "url": "/arweave-id-v2-ui/static/js/24.98c71ab5.chunk.js"
+  },
+  {
+    "revision": "af555b0c1e730e34af2c",
+    "url": "/arweave-id-v2-ui/static/js/25.f1d3e4b1.chunk.js"
+  },
+  {
+    "revision": "fdc42734877cf3a00834",
+    "url": "/arweave-id-v2-ui/static/js/26.c634818d.chunk.js"
+  },
+  {
+    "revision": "6f747102511600c5347b",
+    "url": "/arweave-id-v2-ui/static/js/27.e7cf1fc2.chunk.js"
+  },
+  {
+    "revision": "0ae3ce23373963717ac4",
+    "url": "/arweave-id-v2-ui/static/js/28.a1dff597.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/arweave-id-v2-ui/static/js/28.a1dff597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec8d78bb8555093e3d2",
+    "url": "/arweave-id-v2-ui/static/js/29.5494f92c.chunk.js"
+  },
+  {
+    "revision": "446f2e3c73b70ae061ec",
+    "url": "/arweave-id-v2-ui/static/js/3.5e789587.chunk.js"
+  },
+  {
+    "revision": "e7ef9f13958ddda4e622",
+    "url": "/arweave-id-v2-ui/static/js/30.b1677589.chunk.js"
+  },
+  {
+    "revision": "d32db28b4e05a5e78a81",
+    "url": "/arweave-id-v2-ui/static/js/31.4db769f1.chunk.js"
+  },
+  {
+    "revision": "f04c03a1dd8b562332fe",
+    "url": "/arweave-id-v2-ui/static/js/32.174d04b1.chunk.js"
+  },
+  {
+    "revision": "018f75ab58d947e1f897",
+    "url": "/arweave-id-v2-ui/static/js/33.e5156a6b.chunk.js"
+  },
+  {
+    "revision": "8fbb75842bc0241a2727",
+    "url": "/arweave-id-v2-ui/static/js/34.1fa7db42.chunk.js"
+  },
+  {
+    "revision": "cf22166af4293fb3993c",
+    "url": "/arweave-id-v2-ui/static/js/35.5989a3a4.chunk.js"
+  },
+  {
+    "revision": "579c2da8f656cc9cc0b5",
+    "url": "/arweave-id-v2-ui/static/js/36.c5458342.chunk.js"
+  },
+  {
+    "revision": "56c88ff3e48e3cfcfc3f",
+    "url": "/arweave-id-v2-ui/static/js/37.ee38c09a.chunk.js"
+  },
+  {
+    "revision": "ba82c1e853b4bdf6812e",
+    "url": "/arweave-id-v2-ui/static/js/38.20e29abf.chunk.js"
+  },
+  {
+    "revision": "74038f8b3bf5fe8c1e64",
+    "url": "/arweave-id-v2-ui/static/js/39.dbb8fb72.chunk.js"
+  },
+  {
+    "revision": "9cfe99796a812351be10",
+    "url": "/arweave-id-v2-ui/static/js/4.c852aa60.chunk.js"
+  },
+  {
+    "revision": "8d8e9c60345b3b6685fb",
+    "url": "/arweave-id-v2-ui/static/js/40.5c3c37a2.chunk.js"
+  },
+  {
+    "revision": "ca1919708628938b4027",
+    "url": "/arweave-id-v2-ui/static/js/41.ec84fb92.chunk.js"
+  },
+  {
+    "revision": "c2d848bd6c09663c9c0a",
+    "url": "/arweave-id-v2-ui/static/js/42.ff1bdc5b.chunk.js"
+  },
+  {
+    "revision": "775106869811c19f33bc",
+    "url": "/arweave-id-v2-ui/static/js/43.112b145c.chunk.js"
+  },
+  {
+    "revision": "ca484f0f8d75c0c8e226",
+    "url": "/arweave-id-v2-ui/static/js/44.5e888108.chunk.js"
+  },
+  {
+    "revision": "05986db269b7ebf5be7c",
+    "url": "/arweave-id-v2-ui/static/js/45.c40f0420.chunk.js"
+  },
+  {
+    "revision": "da60f539542ce9976e3d",
+    "url": "/arweave-id-v2-ui/static/js/46.7e9f9388.chunk.js"
+  },
+  {
+    "revision": "a240055736ffab5ac7f7",
+    "url": "/arweave-id-v2-ui/static/js/47.695291d6.chunk.js"
+  },
+  {
+    "revision": "b1b9ece708713618aa91",
+    "url": "/arweave-id-v2-ui/static/js/48.1b850c0b.chunk.js"
+  },
+  {
+    "revision": "620bb86d6b50ac13c7b6",
+    "url": "/arweave-id-v2-ui/static/js/49.035a498b.chunk.js"
+  },
+  {
+    "revision": "fc78df1601a54c43d20c",
+    "url": "/arweave-id-v2-ui/static/js/5.8d8a99c2.chunk.js"
+  },
+  {
+    "revision": "74bc7a6d52dd67c153ba",
+    "url": "/arweave-id-v2-ui/static/js/50.e4ca4f29.chunk.js"
+  },
+  {
+    "revision": "5f51eecaebf62ee8b08b",
+    "url": "/arweave-id-v2-ui/static/js/51.8eecf84a.chunk.js"
+  },
+  {
+    "revision": "93ac476c610a55ba32e4",
+    "url": "/arweave-id-v2-ui/static/js/52.2a385eda.chunk.js"
+  },
+  {
+    "revision": "46d7550259601c922320",
+    "url": "/arweave-id-v2-ui/static/js/53.9a954acd.chunk.js"
+  },
+  {
+    "revision": "17acacc78b42c4ee1754",
+    "url": "/arweave-id-v2-ui/static/js/54.2379fb32.chunk.js"
+  },
+  {
+    "revision": "36e84cf7fc6a4aee6133",
+    "url": "/arweave-id-v2-ui/static/js/55.dc67db9b.chunk.js"
+  },
+  {
+    "revision": "6056fc4cc76b8e4a6478",
+    "url": "/arweave-id-v2-ui/static/js/56.c9520a4e.chunk.js"
+  },
+  {
+    "revision": "63cf762d92e4a50ee14f",
+    "url": "/arweave-id-v2-ui/static/js/57.64a1c7c1.chunk.js"
+  },
+  {
+    "revision": "a9681e7f61c3d0fda1da",
+    "url": "/arweave-id-v2-ui/static/js/58.d647ef43.chunk.js"
+  },
+  {
+    "revision": "9d2f1a26727e92827c4a",
+    "url": "/arweave-id-v2-ui/static/js/59.709688bd.chunk.js"
+  },
+  {
+    "revision": "a9218805cc9d8c38c7a0",
+    "url": "/arweave-id-v2-ui/static/js/6.2415bbc9.chunk.js"
+  },
+  {
+    "revision": "1125a35c875db47c9e64",
+    "url": "/arweave-id-v2-ui/static/js/60.7280a5b8.chunk.js"
+  },
+  {
+    "revision": "7f996c76593c5f839624",
+    "url": "/arweave-id-v2-ui/static/js/61.d4d74d67.chunk.js"
+  },
+  {
+    "revision": "1bfd7c9954a3c48b6ec4",
+    "url": "/arweave-id-v2-ui/static/js/62.a9220f10.chunk.js"
+  },
+  {
+    "revision": "07e21b64d502066b96f6",
+    "url": "/arweave-id-v2-ui/static/js/63.e664abe8.chunk.js"
+  },
+  {
+    "revision": "484be7ef51b3e12a15a4",
+    "url": "/arweave-id-v2-ui/static/js/64.eb22b367.chunk.js"
+  },
+  {
+    "revision": "64e7c96d03de6b2514c3",
+    "url": "/arweave-id-v2-ui/static/js/65.4d88fea8.chunk.js"
+  },
+  {
+    "revision": "c3ba1f9a6ddc65175d80",
+    "url": "/arweave-id-v2-ui/static/js/66.215a485b.chunk.js"
+  },
+  {
+    "revision": "c90d9d2bf11223c354fa",
+    "url": "/arweave-id-v2-ui/static/js/67.2c55c1c9.chunk.js"
+  },
+  {
+    "revision": "675f6abacf8ccd5d20f1",
+    "url": "/arweave-id-v2-ui/static/js/68.285b6031.chunk.js"
+  },
+  {
+    "revision": "bfb740c5b42a724ea39e",
+    "url": "/arweave-id-v2-ui/static/js/69.3b2c26e4.chunk.js"
+  },
+  {
+    "revision": "bbc99f695b42c7de0b13",
+    "url": "/arweave-id-v2-ui/static/js/70.e7874d7c.chunk.js"
+  },
+  {
+    "revision": "933d2683ce54a13ac381",
+    "url": "/arweave-id-v2-ui/static/js/71.9d70f0a8.chunk.js"
+  },
+  {
+    "revision": "3de903313c169ecf6cfa",
+    "url": "/arweave-id-v2-ui/static/js/72.94e2f930.chunk.js"
+  },
+  {
+    "revision": "b56fcd5e3f6f5a494e94",
+    "url": "/arweave-id-v2-ui/static/js/73.f95a3d8f.chunk.js"
+  },
+  {
+    "revision": "ec89adb56dbf8ee2b15c",
+    "url": "/arweave-id-v2-ui/static/js/74.a9816c2c.chunk.js"
+  },
+  {
+    "revision": "ad1d07e8b73ee63c52d0",
+    "url": "/arweave-id-v2-ui/static/js/75.74b2dd90.chunk.js"
+  },
+  {
+    "revision": "8436bcf7f4a6f1e7ae75",
+    "url": "/arweave-id-v2-ui/static/js/76.c271c88b.chunk.js"
+  },
+  {
+    "revision": "4621ba431b39e289285f",
+    "url": "/arweave-id-v2-ui/static/js/77.ee0f2275.chunk.js"
+  },
+  {
+    "revision": "764d871ec438812532f9",
+    "url": "/arweave-id-v2-ui/static/js/78.8ba3234d.chunk.js"
+  },
+  {
+    "revision": "670f451db986339831ef",
+    "url": "/arweave-id-v2-ui/static/js/79.8003f6e5.chunk.js"
+  },
+  {
+    "revision": "0684d1cf0491aca5ffe4",
+    "url": "/arweave-id-v2-ui/static/js/80.361964e0.chunk.js"
+  },
+  {
+    "revision": "f67493c07b86bea64082",
+    "url": "/arweave-id-v2-ui/static/js/81.9558174e.chunk.js"
+  },
+  {
+    "revision": "11a4fff63c1808da0d47",
+    "url": "/arweave-id-v2-ui/static/js/82.852119d7.chunk.js"
+  },
+  {
+    "revision": "31ab94f0d1f86aba321a",
+    "url": "/arweave-id-v2-ui/static/js/83.c9dcacc4.chunk.js"
+  },
+  {
+    "revision": "e80b22599d0f4bcd8768",
+    "url": "/arweave-id-v2-ui/static/js/84.a2f5a790.chunk.js"
+  },
+  {
+    "revision": "7f29035eec442ae2bf80",
+    "url": "/arweave-id-v2-ui/static/js/85.3127d796.chunk.js"
+  },
+  {
+    "revision": "3d0a8ad49f4f0b2d537c",
+    "url": "/arweave-id-v2-ui/static/js/86.cea3b981.chunk.js"
+  },
+  {
+    "revision": "69e29465209eb2d8908c",
+    "url": "/arweave-id-v2-ui/static/js/87.eec101a8.chunk.js"
+  },
+  {
+    "revision": "ddbefd3fffd8d4d54548",
+    "url": "/arweave-id-v2-ui/static/js/88.957689a8.chunk.js"
+  },
+  {
+    "revision": "43d665b2df6764ce15e3",
+    "url": "/arweave-id-v2-ui/static/js/89.5cb0d1a9.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/arweave-id-v2-ui/static/js/89.5cb0d1a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07500f01a499e2f8df92",
+    "url": "/arweave-id-v2-ui/static/js/90.4dc13d4f.chunk.js"
+  },
+  {
+    "revision": "56d2d628840f4ddbf345",
+    "url": "/arweave-id-v2-ui/static/js/91.baadd854.chunk.js"
+  },
+  {
+    "revision": "edd741894a4e2e41d29a",
+    "url": "/arweave-id-v2-ui/static/js/92.f67dea8b.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/arweave-id-v2-ui/static/js/92.f67dea8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf928177bab0f13adfd",
+    "url": "/arweave-id-v2-ui/static/js/main.6a3a8255.chunk.js"
+  },
+  {
+    "revision": "c375365bf5fc036ed1dd",
+    "url": "/arweave-id-v2-ui/static/js/polyfills-css-shim.2c2a3b6f.chunk.js"
+  },
+  {
+    "revision": "02f31a9a987d885c6132",
+    "url": "/arweave-id-v2-ui/static/js/runtime-main.98f52d1b.js"
+  },
+  {
+    "revision": "a7eb535cbeae736ee73156f233d94320",
+    "url": "/arweave-id-v2-ui/static/media/arca.a7eb535c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arweave-id-v2-ui/precache-manifest.60f8f2c44bb42330efc3d49b2b672873.js"
+  "/arweave-id-v2-ui/precache-manifest.d2539ac93b4f9a6ecaac8ce0f4602b27.js"
 );
 
 self.addEventListener('message', (event) => {
